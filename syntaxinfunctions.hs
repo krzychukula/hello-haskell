f465916df010c869801caa76487f9997d6141bc7
krzychukula <krzychukula@gmail.com>
2015-12-25 11:11:32 +0100

    let bindigns

--- syntaxinfunctions.hs
+++ syntaxinfunctions.hs
@@ -144,7 +144,25 @@ calcBmis xs = [  bmi w h | (w, h) <- xs]
 
 -- Let it be
 
+cylinder :: RealFloat a => a -> a -> a
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea  = pi * r ^ 2
+  in  sideArea + 2 * topArea
 
+fourtytwo = 4 * (let a = 9 in a + 1) + 2
+squaretuple = [let square x = x * x in (square 5, square 3, square 2)]
+-- [(25,9,4)]
+
+tupleMultipleLet = (let a = 100; b = 200; c = 300 in a*b*c, let foo="Hey "; bar = "there!" in foo ++ bar)
+-- (6000000,"Hey there!")
+
+sixhundred (let (a,b,c) = (1,2,3) in a+b+c) * 100
+
+calcBmis2 :: RealFloat a => [(a,a)] -> [a]
+calcBmis2 xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]
+
+-- Next: Case expressions
 
 
 --

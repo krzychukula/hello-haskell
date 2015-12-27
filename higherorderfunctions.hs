d95f74a8ce5e5f174e193c704089b4e11e80af30
krzychukula <krzychukula@gmail.com>
2015-12-27 17:59:35 +0100

    maps and filters

--- higherorderfunctions.hs
+++ higherorderfunctions.hs
@@ -38,7 +38,7 @@ flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f = g
   where g x y = f y x
 
-flipsimpler :: (a -> b -> ) -> b -> a -> c
+flipsimpler :: (a -> b -> c) -> b -> a -> c
 flipsimpler f y x = f x y
 
 -- zipWith (flip' div) [2,2..] [10,8,6,4,2]
@@ -46,6 +46,45 @@ flipsimpler f y x = f x y
 
 -- Maps and filters:
 
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x: map' f xs
+
+-- [x+3 | x <- [1,5,3,1,6]] == map (+3) [1,5,3,1,6]
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+  | p x           = x : filter p xs
+  | otherwise     = filter p xs
+  --
+  -- filter even [1,2,3]
+  -- [2]
+
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000,99999..])
+  where p x = x `mod` 3829 == 0
+
+-- find the sum of all odd squares that are smaller than 10,000
+foundSum = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+foundSum2 = sum (takeWhile (<10000) [n^2 | n <- [1..], odd (n^2)])
+
+-- for all starting numbers between 1 and 100, how many chains have a length greater than 15?
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n:chain (n `div` 2)
+  | odd n  = n:chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+  where isLong xs = length xs > 15
+
+-- 66
+
+-- Next: Lambdas
 
 
 

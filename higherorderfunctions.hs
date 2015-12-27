64cf048cc6f31aaca571c9b45275320840b17915
krzychukula <krzychukula@gmail.com>
2015-12-27 20:11:15 +0100

    using lambda \x -> x

--- higherorderfunctions.hs
+++ higherorderfunctions.hs
@@ -86,6 +86,21 @@ numLongChains = length (filter isLong (map chain [1..100]))
 
 -- Next: Lambdas
 
+numLongChains' :: Int
+numLongChains' = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+multipleArguments = zipWith (\a b -> (a * 30 + 3) / b) [5,4,3,2,1] [1,2,3,4,5]
+
+patternMatching = map (\(a,b) -> a + b) [(1,2),(3,4)]
+
+addThree1 :: (Num a) => a -> a -> a -> a
+addThree1 x y z = x + y + z
+-- the same as:
+addThree2 :: (Num a) => a -> a -> a -> a
+addThree2 = \x -> \y -> \z -> x + y + z
+
+fliplambda :: (a -> b -> c) -> b -> a -> c
+fliplambda f = \x y -> f y x
 
 
 

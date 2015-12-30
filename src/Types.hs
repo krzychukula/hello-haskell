499a197d44af88d04a9b7a9a0387dddd5354e742
krzychukula <krzychukula@gmail.com>
2015-12-30 18:04:57 +0100

    Use Point

--- src/Types.hs
+++ src/Types.hs
@@ -1,20 +1,20 @@
 
 module Types
-    (
-    Shape(..)
+    ( Point(..)
+    , Shape(..)
     , surface
     ) where
 
 -- data Bool = False | True
 
-
-data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
 surface :: Shape -> Float
-surface (Circle _ _ r) = pi * r ^ 2
-surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+surface (Circle _ r) = pi * r ^ 2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
--- surface $ Circle 10 20 10
--- 314.15927
--- ghci> surface $ Rectangle 0 0 100 100
+-- surface (Rectangle (Point 0 0) (Point 100 100))  
 -- 10000.0
+-- ghci> surface (Circle (Point 0 0) 24)
+-- 1809.5574

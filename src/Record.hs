3887c2a45a84648aa8ef4713f880c16916170b03
krzychukula <krzychukula@gmail.com>
2016-01-01 20:28:41 +0100

    type parameters

--- src/Record.hs
+++ src/Record.hs
@@ -35,7 +35,7 @@ data Person = Person {  firstName :: String
 
 
 
-data Car = Car { company :: String
+data CarOne = CarOne { company :: String
                 , model :: String
                 , year :: Int
                 } deriving (Show)
@@ -43,6 +43,53 @@ data Car = Car { company :: String
 -- > Car {company="Ford", model="Mustang", year=1967}
 -- Car {company = "Ford", model = "Mustang", year = 1967}
 
+
+-- Type Parameters
+
+
+data Maybe a = Nothing | Just a
+
+data Car a b c = Car { company :: a
+                      , model :: b
+                      , year :: c
+                    } deriving (Show)
+
+tellCarOne :: CarOne -> String
+tellCarOne (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+-- ghci> let stang = Car {company="Ford", model="Mustang", year=1967}
+-- ghci> tellCar stang
+-- "This Ford Mustang was made in 1967"
+
+tellCar :: (Show a) => Car String String a -> String
+tellCar (Car {company = c, model = m, year = y}) = "This " ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+
+-- ghci> tellCar (Car "Ford" "Mustang" 1967)
+-- "This Ford Mustang was made in 1967"
+
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+vectMult :: (Num t) => Vector t -> t -> Vector t
+(Vector i j k) `vectMult` m = Vector (i*m) (j*m) (k*m)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
+
+-- ghci> Vector 3 5 8 `vplus` Vector 9 2 8
+-- Vector 12 7 16
+-- ghci> Vector 3 5 8 `vplus` Vector 9 2 8 `vplus` Vector 0 2 3
+-- Vector 12 9 19
+-- ghci> Vector 3 9 7 `vectMult` 10
+-- Vector 30 90 70
+-- ghci> Vector 4 9 5 `scalarMult` Vector 9.0 2.0 4.0
+-- 74.0
+-- ghci> Vector 2 9 3 `vectMult` (Vector 4 9 5 `scalarMult` Vector 9 2 4)
+-- Vector 148 666 222
 
 
 

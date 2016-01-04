08ca811a333fce922ef76d908bfac81c974ce88c
krzychukula <krzychukula@gmail.com>
2016-01-04 07:54:40 +0100

    recursive data types: binary treee

--- src/RecursiveData.hs
+++ src/RecursiveData.hs
@@ -46,8 +46,40 @@ Empty .++ ys = ys
 
 
 
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a  = Node a (treeInsert x left) right
+  | x > a  = Node a left (treeInsert x right)
 
 
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+  | x == a = True
+  | x < a  = treeElem x left
+  | x > a  = treeElem x right
+
+nums = [8,6,4,1,7,3,5]
+numsTree = foldr treeInsert EmptyTree nums
+
+-- ghci> numsTree
+-- Node 5 (Node 3 (Node 1 EmptyTree EmptyTree) (Node 4 EmptyTree EmptyTree)) (Node 7 (Node 6 EmptyTree EmptyTree) (Node 8 EmptyTree EmptyTree))
+
+-- ghci> 8 `treeElem` numsTree
+-- True
+-- ghci> 100 `treeElem` numsTree
+-- False
+-- ghci> 1 `treeElem` numsTree
+-- True
+-- ghci> 10 `treeElem` numsTree
+-- False
 
 
 ---

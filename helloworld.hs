176f91c321e8a4831c7f944056e653200bcbe424
krzychukula <krzychukula@gmail.com>
2016-01-08 21:21:49 +0100

    simple compiled hello world

---
+++ helloworld.hs
@@ -0,0 +1,7 @@
+-- stack ghc helloworld
+-- [1 of 1] Compiling Main             ( helloworld.hs, helloworld.o )
+-- Linking helloworld ...
+-- ./helloworld
+-- hello, world
+
+main = putStrLn "hello, world"

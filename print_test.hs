0e89378e95077c981e8711326a365d5a6db0514f
krzychukula <krzychukula@gmail.com>
2016-01-09 20:10:30 +0100

    print

---
+++ print_test.hs
@@ -0,0 +1,12 @@
+
+main = do   print True
+            print 2
+            print "haha"
+            print 3.2
+            print [3,4,2]
+-- stack runghc print_test.hs
+-- True
+-- 2
+-- "haha"
+-- 3.2
+-- [3,4,2]

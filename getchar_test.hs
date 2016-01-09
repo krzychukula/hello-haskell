272746e2b1cc92a34209d813dd55ffeeb22a6504
krzychukula <krzychukula@gmail.com>
2016-01-09 20:15:26 +0100

    when

--- getchar_test.hs
+++ getchar_test.hs
@@ -1,11 +1,21 @@
-main = do
-  c <- getChar
-  if c /= ' '
-    then do
-      putChar c
-      main
-    else return ()
+-- main = do
+--   c <- getChar
+--   if c /= ' '
+--     then do
+--       putChar c
+--       main
+--     else return ()
 
 -- stack runghc getchar_test.hs
 -- dsaf dsafdsaf dsaf dsaf dsf saf
 -- dsaf%
+
+
+-- the same with `when` funciton
+import Control.Monad
+
+main = do
+  c <- getChar
+  when (c /= ' ') $ do
+    putChar c
+    main

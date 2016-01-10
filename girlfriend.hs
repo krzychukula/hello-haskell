c1195f036ccff5573da971ed207e85f8ae2d9647
krzychukula <krzychukula@gmail.com>
2016-01-10 12:24:07 +0100

    buffering

--- girlfriend.hs
+++ girlfriend.hs
@@ -11,6 +11,7 @@ main = do
 
 -- main = do
 --   withFile "girlfriend.txt" ReadMode (\handle -> do
+--     hSetBuffering handle $ BlockBuffering (Just 2048)
 --     contents <- hGetContents handle
 --     putStr contents)
 

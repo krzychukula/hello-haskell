488fca9af2cb59e1469f7977b3494235ae0ef229
krzychukula <krzychukula@gmail.com>
2016-01-10 12:14:56 +0100

    readFile

--- girlfriend.hs
+++ girlfriend.hs
@@ -1,9 +1,13 @@
 import System.IO
 
 main = do
-  withFile "girlfriend.txt" ReadMode (\handle -> do
-    contents <- hGetContents handle
-    putStr contents)
+  contents <- readFile "girlfriend.txt"
+  putStr contents
+
+-- main = do
+--   withFile "girlfriend.txt" ReadMode (\handle -> do
+--     contents <- hGetContents handle
+--     putStr contents)
 
 -- main = do
 --   handle <- openFile "girlfriend.txt" ReadMode

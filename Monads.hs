ffd0971d82874cef53abdd4fb747f623d9e8b12b
krzychukula <krzychukula@gmail.com>
2016-01-26 07:22:02 +0100

    Maybe monad

--- Monads.hs
+++ Monads.hs
@@ -18,4 +18,30 @@ applyMaybe (Just x) f = f x
 -- ghci> Just 3 `applyMaybe` \x -> if x > 2 then Just x else Nothing
 -- Just 3
 -- ghci> Just 1 `applyMaybe` \x -> if x > 2 then Just x else Nothing
--- Nothing  
+-- Nothing
+
+class Monad m where
+  return :: a -> m a
+
+  (>>=) :: m a -> (a -> m b) -> m b
+
+  (>>) :: m a -> m b -> m b
+  x >> y = x >>= \_ -> y
+
+  fail :: String -> m a
+  fail msg = error msg
+
+
+instance Monad Maybe where
+  return x = Just x
+  Nothing >>= f = Nothing
+  Just x >>= f = f x
+  fail _ = Nothing
+
+-- ghci> return "WHAT" :: Maybe String
+-- Just "WHAT"
+-- ghci> Just 9 >>= \x -> return (x*10)
+-- Just 90
+-- ghci> Nothing >>= \x -> return (x*10)
+-- Nothing
+ 
